2fbc272df1c3e9a5fb418971757f09b0fb5e9b93
Rachel Bock <rachel.online@gmail.com>
2022-07-20 09:28:52 -0600

    Add Reservations component and constructor to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from './Reservations';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations = []
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +18,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <Reservations />
         </div>
       </div>
     )
